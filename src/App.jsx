11a6847081dcfd86cfbc88e23a9fe270ea879f90
MarjolijnMuller <polle.marjolijn@gmail.com>
2024-11-30 14:55:02 +0100

    opdracht gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+
 function App() {
+    const [light, setLight] = useState(0);
     return (
-        <main className="off">
+        <main className={light === true ? 'on' : 'off'}>
             <section>
                 <div className="dot"></div>
-                <button type="button">Turn on/off</button>
+                <button type="button"
+                        onClick={() => light
+                            ? setLight(false)
+                            : setLight(true)}
+                >
+                    {light ? "Turn off" : "Turn on"}
+                </button>
             </section>
         </main>
     );
